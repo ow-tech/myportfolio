1e3cf2b54796b4036315a180815b5ff97becd08a
ow-tech <wagwanwekon@outloo.com>
2020-05-21 12:46:44 +0300

    initial commit, creation of app, and urls.py for the app

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alex/Documents/moringa-school-projects/python/django/my-portfolio/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
