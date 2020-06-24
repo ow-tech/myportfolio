fcf86fabe20a5263b23f01abb3b6de7e9b22f1d2
ow-tech <wagwanwekon@outloo.com>
2020-06-25 01:10:51 +0300

    staging application for deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =os.environ.get('SECRET_KEY')
+SECRET_KEY=os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =os.environ.get('DEBUG')
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = ['mipotfolio.herokuapp.com','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'favourite.apps.Favouriteos.environ.get',
+    'favourite',
     'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
